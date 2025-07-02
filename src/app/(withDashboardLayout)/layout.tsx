d533296ca01677e58578c9778c81e090298e3203
rashedrk <rashedrk001@gmail.com>
2025-07-03 00:28:03 +0600

    auth issue fix

--- src/app/(withDashboardLayout)/layout.tsx
+++ src/app/(withDashboardLayout)/layout.tsx
@@ -1,7 +1,6 @@
 import DashboardDrawer from "@/components/Dashboard/DashboardDrawer/DashboardDrawer";
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
-  // Authentication is now handled by middleware.ts
   return <DashboardDrawer>{children} </DashboardDrawer>;
 };
 
